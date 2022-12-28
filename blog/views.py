d87c26536bb4495a350d84faac603999b9425f34
Your Name <you@example.com>
2022-12-29 00:45:51 +0700

    update

--- blog/views.py
+++ blog/views.py
@@ -11,20 +11,6 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.detail import SingleObjectMixin
 from history.mixins import ObjectViewMixin
-# import pandas as pd
-# from math import sqrt
-# import numpy as np
-# import matplotlib.pyplot as plt
-# from math import ceil
-
-
-# Create your views here.
-# class LocationListView(ListView):
-#     queryset = Location.objects.all().order_by("-date")
-#     template_name = 'location/location.html'
-#     context_object_name = 'Locations'
-#     paginate_by = 8
-
 
 def locationList(request):
     comment = Location.objects.filter().order_by("-date")
@@ -34,60 +20,98 @@ def locationList(request):
     page_obj = paginator.get_page(page_number)
     return render(request, 'location/location.html',{'page_obj': page_obj, 'category':category})
 
-def search(request):    
-    if 'q'  in request.GET:
-        q = request.GET['q']
-        if q:
-            locations = Location.objects.order_by('-date').filter (Q(Q(name__icontains=q)))
-            location_count = locations.count() 
-    if 'q' and 'qcity' in request.GET:
-        q = request.GET['q']
-        qcity = request.GET['qcity']
-        if q and qcity:
-            locations = Location.objects.order_by('-date').filter (Q(Q(name__icontains=q) and Q(city__icontains=qcity)))
-            location_count = locations.count()
-    if 'qcity' in request.GET:
-        qcity = request.GET['qcity']
-        if qcity:
-            locations = Location.objects.order_by('-date').filter (Q(city__icontains=qcity))
-            location_count = locations.count()
-    if 'qcity' and 'qdistrict' in request.GET:
-        qcity = request.GET['qcity']
+def search(request):   
+    if 'name' in request.GET:
+        name = request.GET['name']
+        locations = Location.objects.order_by('-date').filter(name__icontains=name)
+        location_count = locations.count()
+        
+    elif 'city' in request.GET:
+        city = request.GET['city']
+        locations = Location.objects.order_by('-date').filter(city=city)
+        location_count = locations.count()   
+    
+    if 'name' and 'city' in request.GET:
+        name = request.GET['name']
+        city = request.GET['city']
+        locations = Location.objects.order_by('-date').filter(name__icontains=name,city=city)
+        location_count = locations.count()   
+           
+    elif 'name' and 'city' and 'qdistrict' in request.GET:
+        name = request.GET['name']
+        city = request.GET['city']
         qdistrict = request.GET['qdistrict']
-        if qcity and qdistrict:
-            locations = Location.objects.order_by('-date').filter(Q(Q(city__icontains=qcity) and Q(district__icontains=qdistrict)))
-            location_count = locations.count()
-    if 'qcity' and 'qdistrict' and 'qward' in request.GET:
-        qcity = request.GET['qcity']
+        locations = Location.objects.order_by('-date').filter(name__icontains=name,city=city,district=qdistrict)
+        location_count = locations.count()
+    
+    elif 'name' and 'city' and 'qdistrict' and 'qward' in request.GET:
+        name = request.GET['name']
         qdistrict = request.GET['qdistrict']
         qward = request.GET['qward']
-        if qcity and qdistrict and qward:
-            locations = Location.objects.order_by('-date').filter(Q(Q(city__icontains=qcity) and Q(district__icontains=qdistrict) and Q(wardcommune__icontains=qward)))
-            location_count = locations.count()
-    # if 'qmincost' and   in request.GET and :
-    #     qmincost = request.GET['qmincost']
-    #     if qmincost:
-    #         locations = Location.objects.order_by('-date').filter(Q(mincost__icontains=qmincost))
-    #         location_count = locations.count()
-    # locationsp = Location.objects.all()
-    # if 'qmaxcost'  in request.GET:       
-    #     qmaxcost = request.GET['qmaxcost']
-    #     if qmaxcost:
-    #         locations = Location.objects.order_by('-date').filter(Q(maxcost__icontains=qmaxcost))
-    #         location_count = locations.count()  
-    # locationsp = Location.objects.all() 
-    if 'qmincost' and 'qmaxcost'  in request.GET: 
-        qmincost = request.GET['qmincost']
-        qmaxcost = request.GET['qmaxcost']
-        if qmaxcost and qmincost:
-            locations = Location.objects.order_by('-date').filter(Q(Q(costmin__gte = qmincost) and Q(costmin__lte = qmaxcost) and Q(costmax__lte = qmaxcost)))
-            location_count = locations.count()   
-             
+        city = request.GET['city'] 
+        locations = Location.objects.order_by('-date').filter(name__icontains=name,city=city,district=qdistrict, wardcommune=qward)
+        location_count = locations.count() 
+                            
+    if 'city' and 'qdistrict' in request.GET:
+        city = request.GET['city']
+        qdistrict = request.GET['qdistrict']
+        locations = Location.objects.order_by('-date').filter(city=city,district=qdistrict)
+        location_count = locations.count()
+            
+    if 'city' and 'qdistrict' and 'qward' in request.GET:
+        qdistrict = request.GET['qdistrict']
+        qward = request.GET['qward']
+        city = request.GET['city'] 
+        locations = Location.objects.order_by('-date').filter(city=city,district=qdistrict, wardcommune=qward)
+        location_count = locations.count()   
+    
+    #======= city và code 
+    elif 'city' and 'qcost'  in request.GET: 
+        qcost = request.GET['qcost'] 
+        city = request.GET['city']
+        locations = Location.objects.order_by('-date').filter(city=city,costmax__lte = qcost)
+        location_count = locations.count() 
+               
+    if 'qcost' in request.GET: 
+        qcost = request.GET['qcost']
+        locations = Location.objects.order_by('-date').filter(Q(Q(costmax__lte = qcost)))
+        location_count = locations.count()  
+            
+    if 'name' and 'qcost'  in request.GET: 
+        name = request.GET['name']
+        qcost = request.GET['qcost'] 
+        locations = Location.objects.order_by('-date').filter(name__icontains=name,costmax__lte = qcost)
+        location_count = locations.count()
+        
+    elif 'name' and 'city' and 'qcost' in request.GET: 
+        name = request.GET['name'] 
+        city = request.GET['city']
+        qcost = request.GET['qcost'] 
+        locations = Location.objects.order_by('-date').filter(name__icontains=name,city=city,costmax__lte = qcost)
+        location_count = locations.count()
+        
+    elif 'name' and 'city' and 'qdistrict' and 'qcost' in request.GET: 
+        name = request.GET['name'] 
+        city = request.GET['city']
+        qdistrict = request.GET['qdistrict']
+        qcost = request.GET['qcost']
+        locations = Location.objects.order_by('-date').filter(name__icontains=name,city=city,costmax__lte = qcost,district=qdistrict)
+        location_count = locations.count() 
+      
+    elif 'name' and 'city' and 'qdistrict' and 'qward' and 'qcost' in request.GET: 
+        name = request.GET['name'] 
+        city = request.GET['city']
+        qdistrict = request.GET['qdistrict']
+        qcost = request.GET['qcost']
+        qward = request.GET['qward']
+        locations = Location.objects.order_by('-date').filter(name__icontains=name,city=city,costmax__lte = qcost,district=qdistrict, wardcommune=qward)
+        location_count = locations.count() 
+        
     context = {
         'locations': locations,
         'location_count': location_count,
     }
-    return render(request, 'location/searchlocation.html', context)
+    return render(request, 'location/searchlocation.html',context)
 class detaillocation(ObjectViewMixin, DetailView):  
     model = Location
     template_name = 'location/detaillocation.html'
@@ -157,17 +181,6 @@ class detaillocation(ObjectViewMixin, DetailView):
         else:
             form = RatingForm()
         return render(self, 'location/detaillocation.html', {"detaillocation": detaillocation, "form":form, "ratings":ratings, "image":image, "similarLoca":similarLoca,"rateUser":rateUser})
-
-# def edit_rating(request, pk, rating_id):
-#     detaillocation = Location.objects.get(pk=pk)
-#     rate = Rating.objects.get(detaillocation=detaillocation, id=rating_id)
-#     if request.user == rate.author:
-#         if request.method =="POST":
-#             rate.rating = request.POST['rating']
-#             rate.save()
-#             messages.success(request, "Cập nhật thành công!")
-#             return redirect('/blog/location/' + str(detaillocation.id))
-#         return render(request, 'location/editreview.html',{"detaillocation": detaillocation, "review": rate})
 
 def edit_review(request, pk, review_id):
     detaillocation = Location.objects.get(pk=pk)
